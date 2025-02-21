99d08357379f951036119cc604616d140c29c6d3
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-21 18:53:45 -0300

    Annotation - @DisplayName

--- automated-test-java/src/test/java/jhenriquedsm/SimpleMathTest.java
+++ automated-test-java/src/test/java/jhenriquedsm/SimpleMathTest.java
@@ -1,13 +1,16 @@
 package jhenriquedsm;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+@DisplayName("Test Math Operations in SimpleMath Class")
 public class SimpleMathTest {
     // test[System Under Test]_[Condition or State Change]_[Expected Result]
     @Test
+    @DisplayName("Test 6.2 + 2.0 = 8.2")
     void testSum_When_SixDotTwoIsAddedByTwo_ShouldReturnEightDotTwo() {
         SimpleMath simpleMath = new SimpleMath();
         Double firstNumber = 6.2D;
@@ -24,6 +27,7 @@ public class SimpleMathTest {
     }
 
     @Test
+    @DisplayName("Test 20.0 - 10.0 = 10.0")
     void testSubtraction() {
         SimpleMath simpleMath = new SimpleMath();
         Double firstNumber = 20D;
@@ -40,6 +44,7 @@ public class SimpleMathTest {
     }
 
     @Test
+    @DisplayName("Test 5.0 * 5.0 = 25.0")
     void testMultiplication() {
         SimpleMath simpleMath = new SimpleMath();
         Double firstNumber = 5D;
@@ -56,6 +61,7 @@ public class SimpleMathTest {
     }
 
     @Test
+    @DisplayName("Test 25.0 / 5.0 = 5.0")
     void testDivision() {
         SimpleMath simpleMath = new SimpleMath();
         Double firstNumber = 25D;
@@ -78,6 +84,7 @@ public class SimpleMathTest {
     }
 
     @Test
+    @DisplayName("Test (12.5 + 17.5) / 2 = 15.0")
     void testMean() {
         SimpleMath simpleMath = new SimpleMath();
         Double firstNumber = 12.5;
@@ -94,6 +101,7 @@ public class SimpleMathTest {
     }
 
     @Test
+    @DisplayName("Test squareRoot(25.0) = 5.0")
     void testSquareRoot() {
         SimpleMath simpleMath = new SimpleMath();
         Double number = 25D;
