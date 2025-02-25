39ca75c3229cec5b8838e7a02cb475334e82475d
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-25 16:05:51 -0300

    testing performance in JUnit tests

--- automated-test-java/src/test/java/jhenriquedsm/ArraysCompareTest.java
+++ automated-test-java/src/test/java/jhenriquedsm/ArraysCompareTest.java
@@ -2,8 +2,10 @@ package jhenriquedsm;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 class ArraysCompareTest {
 
@@ -15,5 +17,16 @@ class ArraysCompareTest {
         Arrays.sort(numbers);
 
         Assertions.assertArrayEquals(numbers, expectedArray);
+    }
+
+    @Test
+    @Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+    void testSortPerformance() {
+        int[] numbers = {25, 8, 21, 32, 3};
+        for (int i = 0; i < 1000000; i++){
+            numbers[0] = i;
+            Arrays.sort(numbers);
+        }
+
     }
 }
