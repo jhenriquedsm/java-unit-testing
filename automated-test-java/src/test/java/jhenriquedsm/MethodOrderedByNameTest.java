e7ff4d747e5593e038c4c2f248b7cfa2498c7d60
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-25 18:11:51 -0300

    @TestMethodOrder for ordering by Name

---
+++ automated-test-java/src/test/java/jhenriquedsm/MethodOrderedByNameTest.java
@@ -0,0 +1,29 @@
+package jhenriquedsm;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class MethodOrderedByNameTest {
+    
+    @Test
+    void testC() {
+        System.out.println("Running Test C");
+    }
+
+    @Test
+    void testD() {
+        System.out.println("Running Test D");
+    }
+
+    @Test
+    void testA() {
+        System.out.println("Running Test A");
+    }
+
+    @Test
+    void testB() {
+        System.out.println("Running Test B");
+    }
+}
