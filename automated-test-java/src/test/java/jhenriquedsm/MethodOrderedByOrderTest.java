1b4fab44d69a1eeb5f492d9b08d21bb1df9a00f3
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-25 18:20:56 -0300

    @TestMethodOrder for sorting by Order

--- automated-test-java/src/test/java/jhenriquedsm/MethodOrderedByOrderTest.java
+++ automated-test-java/src/test/java/jhenriquedsm/MethodOrderedByOrderTest.java
@@ -1,28 +1,33 @@
 package jhenriquedsm;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.MethodName.class)
-public class MethodOrderedByNameTest {
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodOrderedByOrderTest {
+    
     @Test
+    @Order(1)
     void testC() {
         System.out.println("Running Test C");
     }
 
     @Test
+    @Order(3)
     void testD() {
         System.out.println("Running Test D");
     }
 
     @Test
+    @Order(2)
     void testA() {
         System.out.println("Running Test A");
     }
 
     @Test
+    @Order(4)
     void testB() {
         System.out.println("Running Test B");
     }
