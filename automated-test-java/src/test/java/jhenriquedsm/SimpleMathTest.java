da03aa8f461c854d8a132cf91d46db48f8cbba46
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-25 15:31:42 -0300

    unit test life cycle

--- automated-test-java/src/test/java/jhenriquedsm/SimpleMathTest.java
+++ automated-test-java/src/test/java/jhenriquedsm/SimpleMathTest.java
@@ -1,21 +1,40 @@
 package jhenriquedsm;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 @DisplayName("Test Math Operations in SimpleMath Class")
 public class SimpleMathTest {
+    SimpleMath simpleMath;
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("Running @BeforeAll method!");
+    }
+
+    @AfterAll
+    static void cleanup() {
+        System.out.println("Running @AfterAll method!");
+    }
+
+    @BeforeEach
+    void beforeEachMethod() {
+        System.out.println("Running @BeforeEach method!");
+        simpleMath = new SimpleMath();
+    }
+
+    @AfterEach
+    void afterEachMethod() {
+        System.out.println("Running @AfterEach method!");
+    }
+
     // test[System Under Test]_[Condition or State Change]_[Expected Result]
     @Test
     @DisplayName("Test 6.2 + 2.0 = 8.2")
     void testSum_When_SixDotTwoIsAddedByTwo_ShouldReturnEightDotTwo() {
         // AAA -> Arrange, Act, Assert
         // Given / Arrange
-        SimpleMath simpleMath = new SimpleMath();
         Double firstNumber = 6.2D;
         Double secondNumber = 2D;
         Double expected = 8.2D;
@@ -34,7 +53,6 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Test 20.0 - 10.0 = 10.0")
     void testSubtraction() {
-        SimpleMath simpleMath = new SimpleMath();
         Double firstNumber = 20D;
         Double secondNumber = 10D;
 
@@ -51,7 +69,6 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Test 5.0 * 5.0 = 25.0")
     void testMultiplication() {
-        SimpleMath simpleMath = new SimpleMath();
         Double firstNumber = 5D;
         Double secondNumber = 5D;
 
@@ -68,7 +85,6 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Test 25.0 / 5.0 = 5.0")
     void testDivision() {
-        SimpleMath simpleMath = new SimpleMath();
         Double firstNumber = 25D;
         Double secondNumber = 5D;
 
@@ -93,7 +109,6 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Test (12.5 + 17.5) / 2 = 15.0")
     void testMean() {
-        SimpleMath simpleMath = new SimpleMath();
         Double firstNumber = 12.5;
         Double secondNumber = 17.5;
 
@@ -110,7 +125,6 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Test squareRoot(25.0) = 5.0")
     void testSquareRoot() {
-        SimpleMath simpleMath = new SimpleMath();
         Double number = 25D;
 
         Double actual = simpleMath.squareRoot(number);
