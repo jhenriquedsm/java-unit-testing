266b10baffcf546de9729006bbbdf8fdb1229caa
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-25 18:02:09 -0300

    @TestMethodOrder for random ordering

---
+++ automated-test-java/src/test/java/jhenriquedsm/MethodOrderedRandomlyTest.java
@@ -0,0 +1,24 @@
+package jhenriquedsm;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodOrderedRandomlyTest {
+
+    @Test
+    void testA() {
+        System.out.println("Running Test A");
+    }
+
+    @Test
+    void testB() {
+        System.out.println("Running Test B");
+    }
+
+    @Test
+    void testC() {
+        System.out.println("Running Test C");
+    }
+}
