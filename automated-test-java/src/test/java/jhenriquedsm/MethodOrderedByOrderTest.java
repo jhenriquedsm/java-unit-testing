7a59d741c9cdb94429a1134cfedee86e6e7309c5
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-25 18:38:06 -0300

    changing the JUnit lifecycle with @TestInstance

--- automated-test-java/src/test/java/jhenriquedsm/MethodOrderedByOrderTest.java
+++ automated-test-java/src/test/java/jhenriquedsm/MethodOrderedByOrderTest.java
@@ -1,35 +1,44 @@
 package jhenriquedsm;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
-@Order(3)
+//@Order(3)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderedByOrderTest {
+
+    StringBuilder actualValue = new StringBuilder("");
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("The actual value is: " + actualValue);
+    }
     
     @Test
     @Order(1)
     void testC() {
         System.out.println("Running Test C");
+        actualValue.append("1");
     }
 
     @Test
     @Order(3)
     void testD() {
         System.out.println("Running Test D");
+        actualValue.append("3");
     }
 
     @Test
     @Order(2)
     void testA() {
         System.out.println("Running Test A");
+        actualValue.append("2");
     }
 
     @Test
     @Order(4)
     void testB() {
         System.out.println("Running Test B");
+        actualValue.append("4");
     }
 }
