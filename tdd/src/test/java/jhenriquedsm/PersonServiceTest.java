f2ae2d984af96ea85d1edf83c390a206bf6a7303
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-27 17:15:28 -0300

    refactoring unit tests

--- tdd/src/test/java/jhenriquedsm/PersonServiceTest.java
+++ tdd/src/test/java/jhenriquedsm/PersonServiceTest.java
@@ -1,22 +1,29 @@
 package jhenriquedsm;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PersonServiceTest {
-    @DisplayName("When Create a Person with Success Should Return a Person Object")
-    @Test
-    void testCreatePerson_WhenSuccess_ShouldReturnPersonObject() {
-    	// Given / Arrange
-        IPersonService service = new PersonService();
-        Person person = new Person(
+    Person person;
+
+    @BeforeEach
+    void setup() {
+        person = new Person(
                 "José",
                 "Henrique",
                 "josehenrique@email.com",
                 "Brasília - DF",
                 "Male"
         );
+    }
+
+    @DisplayName("When Create a Person with Success Should Return a Person Object")
+    @Test
+    void testCreatePerson_WhenSuccess_ShouldReturnPersonObject() {
+    	// Given / Arrange
+        IPersonService service = new PersonService();
 
     	// When / Act
         Person actual = service.createPerson(person);
@@ -30,13 +37,6 @@ public class PersonServiceTest {
     void testCreatePerson_WhenSuccess_ShouldContainsFirstNameInReturnedPersonObject() {
         // Given / Arrange
         IPersonService service = new PersonService();
-        Person person = new Person(
-                "José",
-                "Henrique",
-                "josehenrique@email.com",
-                "Brasília - DF",
-                "Male"
-        );
 
         // When / Act
         Person actual = service.createPerson(person);
