1ecabdf61a0bb8387f17657e8b66794a9caa6b7c
jhenriquedsm <jhsilvamata@gmail.com>
2025-03-05 17:35:21 -0300

    methods then, should and never

--- mockito/src/test/java/jhenriquedsm/business/CourseBusinessMockWithBDDTest.java
+++ mockito/src/test/java/jhenriquedsm/business/CourseBusinessMockWithBDDTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import static org.mockito.BDDMockito.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 public class CourseBusinessMockWithBDDTest {
@@ -64,5 +65,32 @@ public class CourseBusinessMockWithBDDTest {
         verify(mockService, atLeast(1)).deleteCourse("Docker para Amazon AWS Implante Apps Java e .NET com Travis CI");
         verify(mockService, atLeastOnce()).deleteCourse("Spotify Engineering Culture Desmistificado");
         verify(mockService, never()).deleteCourse("Microsserviços do 0 com Spring Cloud, Kotlin e Docker");
+    }
+
+    @DisplayName("Delete Courses not Related to Spring Using Mockito Should call Method deleteCourse V2")
+    @Test
+    void testDeleteCoursesNotRelatedToSpring_UsingMockitoVerify_Should_CallMethod_deleteCourseV2() {
+        // Given / Arrange
+        given(mockService.retrieveCourses("José Henrique")).willReturn(courses);
+
+        // When / Act
+        courseBusiness.deleteCoursesNotRelatedToSpring("José Henrique");
+
+        // Then / Assert
+        then(mockService)
+                .should()
+                .deleteCourse("Agile Desmistificado com Scrum, XP, Kanban e Trello");
+        then(mockService)
+                .should(times(1))
+                .deleteCourse("Kotlin para DEV's Java: Aprenda a Linguagem Padrão do Android");
+        then(mockService)
+                .should(atLeast(1))
+                .deleteCourse("Docker para Amazon AWS Implante Apps Java e .NET com Travis CI");
+        then(mockService)
+                .should(atLeastOnce())
+                .deleteCourse("Spotify Engineering Culture Desmistificado");
+        then(mockService)
+                .should(never())
+                .deleteCourse("Microsserviços do 0 com Spring Cloud, Kotlin e Docker");
     }
 }
