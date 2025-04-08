753a0a9817cc9555e0167d39fa4212362e3f21eb
jhenriquedsm <jhsilvamata@gmail.com>
2025-04-08 16:14:38 -0300

    implementing person entity and repository

---
+++ springREST/src/main/java/jhenriquedsm/SpringREST/repositories/PersonRepository.java
@@ -0,0 +1,10 @@
+package jhenriquedsm.SpringREST.repositories;
+
+import jhenriquedsm.SpringREST.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
