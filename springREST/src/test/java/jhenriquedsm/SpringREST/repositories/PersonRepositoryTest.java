b7207c0f143e9d1a82f559d342711d0498e14760
jhenriquedsm <jhsilvamata@gmail.com>
2025-04-16 16:18:25 -0300

    converting repository tests into intregation tests

--- springREST/src/test/java/jhenriquedsm/SpringREST/repositories/PersonRepositoryTest.java
+++ springREST/src/test/java/jhenriquedsm/SpringREST/repositories/PersonRepositoryTest.java
@@ -1,18 +1,21 @@
 package jhenriquedsm.SpringREST.repositories;
 
+import jhenriquedsm.SpringREST.integrationtests.testcontainers.AbstractIntegrationTest;
 import jhenriquedsm.SpringREST.model.Person;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
-class PersonRepositoryTest {
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PersonRepositoryTest extends AbstractIntegrationTest {
 
     @Autowired
     PersonRepository repository;
